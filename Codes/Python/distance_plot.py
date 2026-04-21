d7de20d7f9de0e1c2e04db811def90de2d975d26
Ahmed Shalash <ahmed02shalash@gmail.com>
2026-04-21 18:20:36 +0200

    added distance plot python code

---
+++ Codes/Python/distance_plot.py
@@ -0,0 +1,23 @@
+import serial
+import matplotlib.pyplot as plt
+
+ser = serial.Serial('COM3', 9600)
+
+distance = []
+
+plt.ion()
+
+while True:
+    line = ser.readline().decode().strip()
+
+    try:
+        _, _, d, _ = map(float, line.split(','))
+        distance.append(d)
+        distance = distance[-100:]
+        plt.clf()
+        plt.plot(distance)
+        plt.title("Distance")
+        plt.pause(0.01)
+
+    except:
+        continue
