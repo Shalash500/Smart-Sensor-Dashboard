bd50aea170c27b866507110d23638f96556a9d57
Ahmed Shalash <ahmed02shalash@gmail.com>
2026-04-22 18:09:36 +0200

    refactored arduino code with strctures and functions

--- Codes/system/src/main.cpp
+++ Codes/system/src/main.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-// Variables
-long duration;
-float distanceCm;
-
 // Define DHT11 pin and type
 #define DHTPIN 3
 #define DHTTYPE DHT11
@@ -16,6 +12,50 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // Define LDR pin
 #define ldr_pin A0
+
+// sensor data struct
+struct SensorData
+{
+  float humidity {};
+  float temperature {};
+  float distance {};
+  int light {};
+};
+
+SensorData read_sensors(){
+  SensorData data;
+
+  // Read humidity and temperature
+  data.humidity = dht.readHumidity();
+  data.temperature = dht.readTemperature();
+
+  // Read distance
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+
+  long duration = pulseIn(echo_pin, HIGH, 30000);
+  data.distance = duration * 0.0343 / 2;
+
+  // Read light denisty
+  data.light = analogRead(ldr_pin);
+
+  return data;
+
+}
+
+void send_serial(SensorData data)
+{
+  Serial.print(data.humidity);
+  Serial.print(",");
+  Serial.print(data.temperature);
+  Serial.print(",");
+  Serial.print(data.distance);
+  Serial.print(",");
+  Serial.println(data.light);
+}
 
 void setup(){
   Serial.begin(9600); // Start serial communication
@@ -30,31 +70,7 @@ void setup(){
 }
 
 void loop() {
-
-  // Read humidity and temperature
-  float humidity = dht.readHumidity();
-  float temperatureC = dht.readTemperature();
-
-  // Read distance
-  digitalWrite(trig_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig_pin, LOW);
-  duration = pulseIn(echo_pin, HIGH, 30000);
-  distanceCm = duration * 0.0343 / 2;
-
-  // Read light denisty
-  int light_density = analogRead(ldr_pin);
-
-  // Sending data to serial (humidity,temperature,distance,light density)
-  Serial.print(humidity);
-  Serial.print(",");
-  Serial.print(temperatureC);
-  Serial.print(",");
-  Serial.print(distanceCm);
-  Serial.print(",");
-  Serial.println(light_density);
-
+  SensorData data = read_sensors();
+  send_serial(data);
   delay(200);
 }
